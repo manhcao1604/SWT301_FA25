193fa02f1a350d91d9a2e5e5a616715d2fb7abea
de190965 <manhlyabc2005@gmail.com>
2025-10-16 21:16:13 +0700

    Thêm Lab4

--- Lab4_SWT301/src/main/java/caoducmanhde190965/example/SQLInjectionExample.java
+++ Lab4_SWT301/src/main/java/caoducmanhde190965/example/SQLInjectionExample.java
@@ -1,19 +1,9 @@
 package caoducmanhde190965.example;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class SQLInjectionExample {
-    public static void executeSafeQuery(Connection conn, String userInput) throws SQLException {
-
-        String sql = "SELECT * FROM users WHERE username = ?";
-
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, userInput);
-
-            System.out.println("Executing query safely...");
-        }
+    public static void main(String[] args) {
+        String userInput = "' OR '1'='1";
+        String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+        System.out.println("Executing query: " + query);
     }
 }
